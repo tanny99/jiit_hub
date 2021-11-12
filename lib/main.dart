ea47256602c1d06015b970cae8d0b406a99668a7
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2021-11-12 17:50:17 +0530

    LogIn and Logout Feature also added(2)

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,29 @@ import 'screens/LoginPage.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  // Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            print("Error");
+          }
+          if(snapshot.connectionState == ConnectionState.done)
+            {return LoginPage();}
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
-
+// child: LoginPage()
--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -9,7 +9,8 @@ import 'package:http/http.dart' as http;
 import 'package:jiit_hub/responsive_constants.dart';
 import 'package:toast/toast.dart';
 import 'dart:convert';
-// var data;
+import 'package:jiit_hub/screens/Methods.dart';
+var data;
 
 
 
@@ -40,12 +41,18 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController EnrollmentController=TextEditingController();
   final TextEditingController PasswordController=TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      body: isLoading? Center(
+        child: Container(
+          child: CircularProgressIndicator(),
+        ),
+      )
+          : SingleChildScrollView(
         child: Column(
           children: [
             AppBar(
@@ -128,19 +135,30 @@ class _LoginPageState extends State<LoginPage> {
                       style: TextStyle(fontSize: 20,),
                     ),
                     onPressed: ()async {
-                      // int i=await LoginUser(EnrollmentController.text, PasswordController.text);
-                      // if(i==1){
-                      //   Toast.show("Login Successfully! Welcome!!", context,
-                      //       duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
-                      //   Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
-                      // }
-                      // else{
-                      //   Toast.show("Not an Authorized Member!", context,
-                      //       duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
-                      // }
-                      // print(await LoginUser(EnrollmentController.text, PasswordController.text));
-                      // print(EnrollmentController.text);
-                      // print(PasswordController.text);
+                        if(EnrollmentController.text.isNotEmpty &&
+                          PasswordController.text.isNotEmpty) {
+                            setState(() {
+                              isLoading = true;
+                            });
+
+                            LogIn(EnrollmentController.text, PasswordController.text)
+                            .then((user) {
+                              if(user != null) {
+                                print("Login Successful");
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
+                              } else{
+                                print("Login failed");
+                                setState(() {
+                                  isLoading = false;
+                                });
+                              }
+                            });
+                        } else{
+                          print("Please fill the form correctly");
+                        }
                     },
                   ),
                 ),
