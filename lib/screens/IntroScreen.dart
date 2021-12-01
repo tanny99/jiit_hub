677fcbf790489f9ae9bb2782e27eaafacf279299
tanny99 <tanishkgupta10@gmail.com>
2021-12-01 16:08:01 +0530

    Intro Screens Added.

--- lib/screens/IntroScreen.dart
+++ lib/screens/IntroScreen.dart
@@ -19,7 +19,7 @@ class IntroScreen extends StatefulWidget {
 // ------------------ Custom config ------------------
 class IntroScreenState extends State<IntroScreen> {
   List<Slide> slides = [];
-
+//
   @override
   void initState() {
     super.initState();
